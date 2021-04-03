453086a59b7c2a082d62c97369a8c5213c278549
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-03 16:28:10 -0700

    31.6 Making the Toolbar Fragment Talk to the Activity. Have not been tested yet.

--- (Java)AndroidStudio4DevelopmentEssentials/app/src/main/java/com/toby/fragmentexample/MainActivity.java
+++ (Java)AndroidStudio4DevelopmentEssentials/app/src/main/java/com/toby/fragmentexample/MainActivity.java
@@ -1,14 +1,19 @@
 package com.toby.fragmentexample;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements ToolbarFragment.ToolbarListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onButtonClick(int fontSize, String text) {
+
     }
 }
--- (Java)AndroidStudio4DevelopmentEssentials/app/src/main/java/com/toby/fragmentexample/ToolbarFragment.java
+++ (Java)AndroidStudio4DevelopmentEssentials/app/src/main/java/com/toby/fragmentexample/ToolbarFragment.java
@@ -1,5 +1,6 @@
 package com.toby.fragmentexample;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,35 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ToolbarFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ToolbarFragment extends Fragment {
+public class ToolbarFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
+
+    private static int seekValue = 10;
+    private static EditText editText;
+
+    ToolbarListener activityCallBack;
+
+    public interface ToolbarListener {
+        public void onButtonClick(int position, String text);
+    }
+
+    @Override public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            activityCallBack = (ToolbarListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+            + " must implement ToolbarListener");
+        }
+    }
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +82,39 @@ public class ToolbarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_toolbar, container, false);
+        View view = inflater.inflate(R.layout.fragment_toolbar, container, false);
+        editText = view.findViewById(R.id.editText1);
+        final SeekBar seekBar = view.findViewById(R.id.seekBar1);
+        seekBar.setOnSeekBarChangeListener(this);
+        final Button button = view.findViewById(R.id.button1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClicked(v);
+            }
+        });
+        return view;
+    }
+
+    public void buttonClicked(View view) {
+        activityCallBack.onButtonClick(seekValue, editText.getText().toString());
+
+    }
+
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        seekValue = progress;
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
     }
 }
