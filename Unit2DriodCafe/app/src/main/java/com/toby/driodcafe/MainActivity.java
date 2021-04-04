100a3daf173c54ef36adae8ce1980a3d1f41e4ab
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-03 20:55:15 -0700

    hmmm not sure why android duplicated the FragmentExample...this is so strange. other that this, finished Task 2: Add onClick methods for images. And as expected... need to debug. The click is not working. I think fragment does not like onClick method.

--- Unit2DriodCafe/app/src/main/java/com/toby/driodcafe/MainActivity.java
+++ Unit2DriodCafe/app/src/main/java/com/toby/driodcafe/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -52,5 +53,30 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Shows a message that the donut image was clicked.
+     */
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * Shows a message that the ice cream sandwich image was clicked.
+     */
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.icecream_order_message));
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked.
+     */
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
     }
 }
