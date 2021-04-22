f43f45d9649c1328c7725fdf6e6a1aa8fc8b8c6a
tobyatgithub <toby.fangyuan@gmail.com>
2021-04-22 01:27:50 -0700

    ready for testing users fragment display.

---
+++ firebaseChatExmaple/app/src/main/java/com/toby/firebasechatexample/Adapter/UserAdapter.java
@@ -0,0 +1,65 @@
+package com.toby.firebasechatexample.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.toby.firebasechatexample.Model.Users;
+import com.toby.firebasechatexample.R;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private List<Users> mUsers;
+    private Context context;
+
+    public UserAdapter(Context context, List<Users> mUsers) {
+        this.context = context;
+        this.mUsers = mUsers;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Users users = mUsers.get(position);
+        holder.username.setText(users.getUsername());
+
+        if (users.getImageURL().equals("default")) {
+            holder.imageView.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(context).load(users.getImageURL()).into(holder.imageView);
+
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView username;
+        private ImageView imageView;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.useritem_userName);
+            imageView = itemView.findViewById(R.id.useritem_imageView);
+        }
+    }
+}
